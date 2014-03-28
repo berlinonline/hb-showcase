8dcd6500a731fe50d60d5f77bdcdb046f730021c
Steffen Gransow <graste@mivesto.de>
2014-03-28 22:14:14 +0100

    change message as developer is in ~ after init_cms

--- applications/cms/configure-project.sh
+++ applications/cms/configure-project.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 cd applications/cms/honeybee
 make install
 echo ''
-echo 'You may now create your master admin account via:'
+echo 'You may now create your master admin account from the honeybee folder:'
 echo ''
 echo 'bin/cli user.create -username admin -email admin@example.com'
 echo ''
