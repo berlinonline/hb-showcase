0889b20c624256ff0ec8870ba9a1095e2e1e10d4
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2014-06-23 12:27:30 +0200

    djusted java and elasticsearch manifests

---
+++ development/project/modules/java/manifests/openjdk.pp
@@ -0,0 +1,5 @@
+class java::openjdk {
+    package { 'openjdk-7-jdk':
+        ensure => installed
+    }
+}
---
+++ development/project/modules/java/manifests/openjre.pp
@@ -0,0 +1,5 @@
+class java::openjre {
+    package { 'openjdk-7-jre':
+        ensure => installed
+    }
+}
